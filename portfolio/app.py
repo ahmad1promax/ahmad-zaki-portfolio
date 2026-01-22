a5791f2993fadbb5a52fe09107019703c5a08675
ahmad1promax <a7med1.zaki@gmail.com>
2026-01-22 22:59:53 +0300

    Update app.py

--- portfolio/app.py
+++ portfolio/app.py
@@ -2453,6 +2453,7 @@ if __name__ == '__main__':
     print("✨" * 50)
     
 
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
